fa59786db6bc473b6bad87e3c762cf0bbc1ae013
Asekseria <sekseriaankit0657@gmail.com>
2026-03-21 13:46:09 +0530

    docker

--- flask_app.py
+++ flask_app.py
@@ -58,4 +58,4 @@ def predict():
         return f"<h1>Result: {result}</h1><br><a href='/'>Go Back</a>"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host="0.0.0.0", port=8080) #local host and local ip
